79fd1b367e37baa9605f0c6f196e3df1d37faedd
zhiming.li <627535195@qq.com>
2018-04-26 15:22:52 +0800

    对接实盘易

--- src/main/java/net/cloudstu/sg/util/ShiPanEUtil.java
+++ src/main/java/net/cloudstu/sg/util/ShiPanEUtil.java
@@ -1,6 +1,7 @@
 package net.cloudstu.sg.util;
 
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import net.cloudstu.sg.app.RootConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.HttpEntity;
@@ -11,15 +12,66 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public abstract class ShiPanEUtil {
 
     public static String API_URL = "http://39.105.55.173:8888/%s";
 
+    /**
+     * 定量定价卖出
+     *
+     * @param code   代码
+     * @param amount 股数
+     * @param price  价格
+     */
+    public static void sell(String code, int amount, double price) {
+        RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
+        String url = String.format(API_URL, "orders");
+
+        Map map = new HashMap(8);
+        map.put("action", "SELL");
+        map.put("symbol", code);
+        map.put("priceType", 0);
+        map.put("type", "LIMIT");
+        map.put("price", price);
+        map.put("amount", amount);
+
+        HttpEntity<String> request = new HttpEntity<>(JSON.toJSONString(map), getHeaders());
+        String result = restTemplate.postForObject(url, request, String.class);
+    }
+
+    /**
+     * 五档即时成交剩余撤销 卖出
+     *
+     * @param code   代码
+     * @param amount 股数
+     */
+    public static void sell(String code, int amount) {
+        RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
+        String url = String.format(API_URL, "orders");
+
+        Map map = new HashMap(8);
+        map.put("action", "SELL");
+        map.put("symbol", code);
+        map.put("priceType", 4);
+        map.put("type", "MARKET");
+        map.put("amount", amount);
+
+        HttpEntity<String> request = new HttpEntity<>(JSON.toJSONString(map), getHeaders());
+        String result = restTemplate.postForObject(url, request, String.class);
+    }
+
+    /**
+     * 定量定价买入
+     *
+     * @param code   代码
+     * @param amount 股数
+     */
     public static void buy(String code, int amount, double price) {
         RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
         String url = String.format(API_URL, "orders");
 
-        Map map = new HashMap();
+        Map map = new HashMap(8);
         map.put("action", "BUY");
         map.put("symbol", code);
         map.put("priceType", 0);
@@ -27,11 +79,36 @@ public abstract class ShiPanEUtil {
         map.put("price", price);
         map.put("amount", amount);
 
-        String param = JSON.toJSONString(map);
-        HttpEntity<String> formEntity = new HttpEntity<String>(param, getHeaders());
-        String result = restTemplate.postForObject(url, formEntity, String.class);
+        HttpEntity<String> request = new HttpEntity<>(JSON.toJSONString(map), getHeaders());
+        String result = restTemplate.postForObject(url, request, String.class);
     }
 
+    /**
+     * 五档即时成交剩余撤销 买入
+     *
+     * @param code   代码
+     * @param amount 股数
+     */
+    public static void buy(String code, int amount) {
+        RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
+        String url = String.format(API_URL, "orders");
+
+        Map map = new HashMap(8);
+        map.put("action", "BUY");
+        map.put("symbol", code);
+        map.put("priceType", 4);
+        map.put("type", "MARKET");
+        map.put("amount", amount);
+
+        HttpEntity<String> request = new HttpEntity<>(JSON.toJSONString(map), getHeaders());
+        String result = restTemplate.postForObject(url, request, String.class);
+        System.out.println(request);
+
+    }
+
+    /**
+     * 撤销所有委托
+     */
     public static void cancelAll() {
         RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
         String url = String.format(API_URL, "orders");
@@ -49,9 +126,6 @@ public abstract class ShiPanEUtil {
 
     public static void main(String[] args) {
         new AnnotationConfigApplicationContext(RootConfig.class);
-
-//        ShiPanEUtil.cancelAll();
-
-        ShiPanEUtil.buy("000001", 200, 11);
+        ShiPanEUtil.buy("600381", 500);
     }
 }
