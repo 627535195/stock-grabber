9cb50bd93aff32f5101fce00ad69693ee153d3f0
zmli <627535195@qq.com>
2018-04-26 09:24:31 +0800

    实盘易对接

--- src/main/java/net/cloudstu/sg/util/ShiPanEUtil.java
+++ src/main/java/net/cloudstu/sg/util/ShiPanEUtil.java
@@ -1,9 +1,7 @@
 package net.cloudstu.sg.util;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import net.cloudstu.sg.app.RootConfig;
-import net.cloudstu.sg.util.sinastock.SinaStockClient;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -17,44 +15,43 @@ public abstract class ShiPanEUtil {
 
     public static String API_URL = "http://39.105.55.173:8888/%s";
 
-    public static void buy(String code, int amount) {
+    public static void buy(String code, int amount, double price) {
         RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
         String url = String.format(API_URL, "orders");
-
-        HttpHeaders headers = new HttpHeaders();
-        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
-        headers.setContentType(type);
-        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
-
-//        {
-//            "action": "BUY",
-//                "symbol" : "000001",
-//                "type": "LIMIT",
-//                "priceType" : 0,
-//                "price" : 11.69,
-//                "amountProportion" : "1/3"
-//        }
 
         Map map = new HashMap();
         map.put("action", "BUY");
         map.put("symbol", code);
         map.put("priceType", 0);
         map.put("type", "LIMIT");
-        map.put("price", 11);
+        map.put("price", price);
         map.put("amount", amount);
 
         String param = JSON.toJSONString(map);
-        System.out.println(param);
-        HttpEntity<String> formEntity = new HttpEntity<String>(param, headers);
-
+        HttpEntity<String> formEntity = new HttpEntity<String>(param, getHeaders());
         String result = restTemplate.postForObject(url, formEntity, String.class);
-        System.out.println(result);
+    }
 
+    public static void cancelAll() {
+        RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
+        String url = String.format(API_URL, "orders");
+
+        restTemplate.delete(url);
+    }
+
+    private static HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+        return headers;
     }
 
     public static void main(String[] args) {
         new AnnotationConfigApplicationContext(RootConfig.class);
 
-        ShiPanEUtil.buy("000001", 200);
+//        ShiPanEUtil.cancelAll();
+
+        ShiPanEUtil.buy("000001", 200, 11);
     }
 }
