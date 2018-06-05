97b075d346de15058cf2bd1a06d6a832f839f896
zmli <627535195@qq.com>
2018-06-05 08:31:04 +0800

    实盘易对接

--- src/main/java/net/cloudstu/sg/grab/StockHoldRepo.java
+++ src/main/java/net/cloudstu/sg/grab/StockHoldRepo.java
@@ -6,6 +6,7 @@ import net.cloudstu.sg.dao.StockHoldDao;
 import net.cloudstu.sg.entity.StockHoldModel;
 import net.cloudstu.sg.util.ShiPanEUtil;
 import net.cloudstu.sg.util.SpringUtil;
+import net.cloudstu.sg.util.ThreadUtil;
 import net.cloudstu.sg.util.WxmpSender;
 import net.cloudstu.sg.util.sinastock.StockRealTimeInfoTemplate;
 import net.cloudstu.sg.util.sinastock.data.StockData;
@@ -16,6 +17,7 @@ import org.springframework.util.StringUtils;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 持仓处理
@@ -88,6 +90,31 @@ public class StockHoldRepo {
         }
 
     }
+
+    /**
+     * 卖出没有涨停的股票
+     */
+    public static void sellAnyway() {
+        StockHoldDao stockHoldDao = SpringUtil.getBean(StockHoldDao.class);
+        List<StockHoldModel> stockHolds = stockHoldDao.selectAll();
+
+        for (StockHoldModel stockHold : stockHolds) {
+            if(stockHold.getAvailableHold()>0 ) {
+                StockRealTimeInfoTemplate.get(stockHold.getCode(), response -> {
+                    StockData data = response.getData();
+
+                    //没有涨停
+                    if (data.getSwing() < 9.85) {
+                        ShiPanEUtil.sell(stockHold.getCode(), stockHold.getAvailableHold());
+                    }
+
+                });
+
+            }
+            ThreadUtil.sleep(2, TimeUnit.SECONDS);
+        }
+    }
+
 
     private static String getLargeUp(StockHoldModel stockHold) {
         return String.format("【{}】大幅拉升！", stockHold.getName());
--- src/main/java/net/cloudstu/sg/web/listener/StockHoldListener.java
+++ src/main/java/net/cloudstu/sg/web/listener/StockHoldListener.java
@@ -1,6 +1,9 @@
 package net.cloudstu.sg.web.listener;
 
+import net.cloudstu.sg.grab.MonitoredStockLoader;
+import net.cloudstu.sg.grab.ScreamStockRepo;
 import net.cloudstu.sg.grab.StockHoldRepo;
+import net.cloudstu.sg.util.ShiPanEUtil;
 import net.cloudstu.sg.util.SimpleTimer;
 import net.cloudstu.sg.util.TransactionTimeUtil;
 
@@ -22,13 +25,20 @@ public class StockHoldListener implements ServletContextListener {
         SimpleTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                if(TransactionTimeUtil.isTransactionTime()) {
+                if (TransactionTimeUtil.isTransactionTime()) {
                     StockHoldRepo.refreshHoldStocks();
                 }
             }
-        }, 10000L, 10*60*1000L);
+        }, 10000L, 10 * 60 * 1000L);
 
 
+        //每天的09：45：00卖掉没有涨停的股票
+        SimpleTimer.scheduleAtTime(new TimerTask() {
+            @Override
+            public void run() {
+                StockHoldRepo.sellAnyway();
+            }
+        }, 9, 45);
 
 //        SimpleTimer.scheduleAtFixedRate(new TimerTask() {
 //            @Override
